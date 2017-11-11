b1e93293807182bcf282c3034404da7fbf8da9b9
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-11 18:33:25 -0500

    updated html with bootstrap. removed css.style in lieu of bootstrap. pseudo code js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//array of CPU letters
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Setting user score to 0 vs CPU
+
+//CPU selects letter from Array
+
+//Allows user to randomly select
+
+//If loss, deducts from chance
+
+//If loss, add to lose count
+
+//If win, add to win count
+
