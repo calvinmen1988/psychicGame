f87b16a57a6564c9c59a2699d4d9d2f69202b3d1
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-11 18:53:42 -0500

    updated index. js array updated.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,22 @@
-//array of CPU letters
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//array of CPU letter
+var cpuChoose = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"]
 
-//Setting user score to 0 vs CPU
+//Setting user score to 0 
+var wins = 0;
+var losses = 0;
+var guesses = 10;
 
 //CPU selects letter from Array
+var computerChoice = cpuChoose[Math.floor(Math.random() * cpuChoose.length)];
+
+    console.log(computerChoice)
 
 //Allows user to randomly select
 
 //If loss, deducts from chance
 
+    //If chance reaches 0 game ends.
+
 //If loss, add to lose count
 
 //If win, add to win count
-
