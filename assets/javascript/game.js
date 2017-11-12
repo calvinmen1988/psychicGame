b16d13ec5db97e8606d6c4818fa95635fdf7eafd
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-11 20:18:15 -0500

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,38 @@
 //array of CPU letter
 var cpuChoose = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"]
 
-//Setting user score to 0 
+//var for wins, losses, guesses
 var wins = 0;
 var losses = 0;
 var guesses = 10;
+var guessesLeft=10;
 
 //CPU selects letter from Array
-var computerChoice = cpuChoose[Math.floor(Math.random() * cpuChoose.length)];
+var computerChoice = cpuChoose[Math.floor(Math.random()+cpuChoose.length)];
 
-    console.log(computerChoice)
+    console.log(cpuChoose);
 
-//Allows user to randomly select
+//Allows user to select and determines if user guess correctly or not. If wrong, deducts from guesses left. If out of guesses, add to losses count.
 
-//If loss, deducts from chance
+document.onkeypress = function(event) {
+    var userGuess = event.key;
 
-    //If chance reaches 0 game ends.
+        if (userGuess === computerChoice) {
+            wins++;
+            } else {
+                guesses--;
+                }
 
-//If loss, add to lose count
+        if (guesses = 0) {
+                losses++;
+                }
+        
+    display ();
+            }
+// Counts are update
 
-//If win, add to win count
+function display () {
+            documents.getElementById("wins") = "Wins: " + wins;
+            document.getElementById("losses") = "Losses: " + losses;
+            document.getElementById("guesses") = "Guesses Left: " + guesses;
+}
