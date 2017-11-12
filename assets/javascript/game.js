7831668797fe5114fb8f0e85445346a4e75303bf
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-12 10:34:25 -0500

    update index and .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,39 +4,39 @@ var cpuChoose = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 //var for wins, losses, guesses starting at 0
 var wins = 0;
 var losses = 0;
-var guesses = 10;
-var guessesLeft=10;
+var guessesLeft = 9;
+var guessesSoFar = [];
 
 //CPU selects letter from Array
-var computerChoice = cpuChoose[Math.floor(Math.random()*cpuChoose.length)];
+var computerChoice = cpuChoose[Math.floor(Math.random() * cpuChoose.length)];
 
-    console.log(computerChoice);
+console.log(computerChoice);
 
 //Allows user to select and determines if user guess correctly or not. If wrong, deducts from guesses left. If out of guesses, add to losses count.
 
-document.onkeyup = function() {
-   
+document.onkeyup = function () {
+
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-        if (userGuess === computerChoice) {
-            wins++;
-            alert("True Winner!")}
-             else {
-                guesses--;
-                }
+    if (userGuess === computerChoice) {
+        wins++;
+        alert("True Winner!")
+    }
+    else {
+        guessesLeft--;
+    }
 
-            if (guesses = 0) {
-                losses++;
-                alert("Take the L!");
-                }
-        }
-                
-
-        
-//Counts are update
-
-function display () {
-        documents.getElementById("wins") = "Wins: " + wins;
-        document.getElementById("losses") = "Losses: " + losses;
-        document.getElementById("guesses") = "Guesses Left: " + guesses;
+    if (guessesLeft = 0) {
+        losses++;
+        alert("Take the L!");
+    }
 }
+
+
+// Displaying progress to HTML
+
+var html = "<h1>The Psychic Game</h1>" + "<h2>Guess what letter I\'m thinking of</h2>" + "<li>Wins: " + wins + "</li>" + "<li>Losses: " + losses + "</li>" + "<li>Guesses Left: " + guessesLeft + "</li>" + "<li>Your guesses so far: " + guessesSoFar + "</li>";
+
+// place html into the game ID
+
+document.querySelector("#coreGame").innerHTML = html;
