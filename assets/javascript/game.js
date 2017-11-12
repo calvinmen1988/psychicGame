470eea0ba4885e292407b4e251d75ea28ef71dd0
calvinmen1988 <calvinmendoza88@gmail.com>
2017-11-11 21:01:15 -0500

    updated .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,44 @@
 //array of CPU letter
-var cpuChoose = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"]
+var cpuChoose = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//var for wins, losses, guesses
+//var for wins, losses, guesses starting at 0
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 var guessesLeft=10;
 
 //CPU selects letter from Array
-var computerChoice = cpuChoose[Math.floor(Math.random()+cpuChoose.length)];
+var computerChoice = cpuChoose[Math.floor(Math.random()*cpuChoose.length)];
 
-    console.log(cpuChoose);
+    console.log(computerChoice);
 
 //Allows user to select and determines if user guess correctly or not. If wrong, deducts from guesses left. If out of guesses, add to losses count.
 
-document.onkeypress = function(event) {
-    var userGuess = event.key;
+document.onkeyup = function() {
+   
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
         if (userGuess === computerChoice) {
             wins++;
-            } else {
+            alert("True Winner!")}
+             else {
                 guesses--;
                 }
 
-        if (guesses = 0) {
+            if (guesses = 0) {
                 losses++;
+                alert("Take the L!");
                 }
+        }
+                
+
         
-    display ();
-            }
-// Counts are update
+
+        
+//Counts are update
 
 function display () {
-            documents.getElementById("wins") = "Wins: " + wins;
-            document.getElementById("losses") = "Losses: " + losses;
-            document.getElementById("guesses") = "Guesses Left: " + guesses;
+        documents.getElementById("wins") = "Wins: " + wins;
+        document.getElementById("losses") = "Losses: " + losses;
+        document.getElementById("guesses") = "Guesses Left: " + guesses;
 }
